778fd5728801fbe68e370c02bd58030755c523f9
Nikolay Khabarov <2xl@mail.ru>
2017-04-04 15:25:19 +0300

    refactoring, make class alive and reconnect on disconnects, adapter removing etc

--- blue.py
+++ blue.py
@@ -25,24 +25,31 @@ class HFPException(bluetooth.btcommon.BluetoothError):
 class HFPDevice:
 	def __init__(self, addr):
 		self.audio = None
+		self.hfp = None
 		self.addr = addr
-		channel = self._find_channel(addr)
-		if not channel:
-			raise HFPException('HSP/HFP not found')
-		logging.info('HSP/HFP found on RFCOMM channel ' + str(channel))
-
-		self.hfp = bluetooth.BluetoothSocket(bluetooth.RFCOMM)
-		try:
-			self.hfp.connect((addr, channel))
-		except bluetooth.btcommon.BluetoothError as e:
-			self.hfp.close()
-			raise HFPException('Failed to establish ervice level connection: ' + str(e))
-		self.hfp.settimeout(HFP_TIMEOUT)
-		self.pt = threading.Thread(target=self.parse_channel)
+		self.pt = threading.Thread(target=self._worker_loop)
 		self.pt.start()
-		logging.info('HSP/HFP service level connection is established')
 
-	def parse_channel(self):
+	def _worker_loop(self):
+		logging.info('HFPDevice class is initialised')
+		while self.pt:
+			self._find_channel()
+			if not self.channel:
+				time.sleep(HFP_TIMEOUT)
+				continue
+			logging.info('HSP/HFP found on RFCOMM channel ' + str(self.channel))
+			self._connect_service_level()
+			if not self.hfp:
+				time.sleep(HFP_TIMEOUT)
+				continue
+			try:
+				self._parse_channel()
+			except bluetooth.btcommon.BluetoothError as e:
+				logging.warning('Service level connection disconnected: ' + str(e))
+				time.sleep(HFP_TIMEOUT)
+			self._cleanup()
+
+	def _parse_channel(self):
 		audio_time = time.time() + HFP_CONNECT_AUDIO_TIMEOUT
 		sevice_notice = True
 		while self.pt:
@@ -73,49 +80,64 @@ class HFPDevice:
 					if sevice_notice:
 						logging.warning('Service connection timed out, try audio anyway...')
 						sevice_notice = False
-					self._connect_audio();
+					self._connect_audio()
+
+	def _connect_service_level(self):
+		hfp = bluetooth.BluetoothSocket(bluetooth.RFCOMM)
+		try:
+			hfp.connect((self.addr, self.channel))
+		except bluetooth.btcommon.BluetoothError as e:
+			hfp.close()
+			logging.warning('Failed to establish service level connection: ' + str(e))
+			return
+		hfp.settimeout(HFP_TIMEOUT)
+		logging.info('HSP/HFP service level connection is established')
+		self.hfp = hfp
 
 	def _connect_audio(self):
-		if not self.audio:
-			audio = bluetooth.BluetoothSocket(bluetooth.SCO)
-			# socket config
-			opt = struct.pack ("H", BT_VOICE_CVSD_16BIT)
-			audio.setsockopt(SOL_BLUETOOTH, BT_VOICE, opt)
-			try:
-				audio.connect((self.addr,))
-			except bluetooth.btcommon.BluetoothError as e:
-				audio.close()
-				logging.info('Failed to establish audio connection: ' + str(e))
-				return
-			opt = audio.getsockopt(SOL_SCO, SCO_OPTIONS, 2)
-			self.mtu = struct.unpack('H', opt)[0]
-			logging.info('Audio connection is established, mtu = ' + str(self.mtu))
-			self.audio = audio
+		audio = bluetooth.BluetoothSocket(bluetooth.SCO)
+		# socket config
+		opt = struct.pack ("H", BT_VOICE_CVSD_16BIT)
+		audio.setsockopt(SOL_BLUETOOTH, BT_VOICE, opt)
+		try:
+			audio.connect((self.addr,))
+		except bluetooth.btcommon.BluetoothError as e:
+			audio.close()
+			logging.info('Failed to establish audio connection: ' + str(e))
+			return
+		opt = audio.getsockopt(SOL_SCO, SCO_OPTIONS, 2)
+		self.mtu = struct.unpack('H', opt)[0]
+		self.audio = audio
+		logging.info('Audio connection is established, mtu = ' + str(self.mtu))
 
-	def _find_channel(self, addr):
+	def _find_channel(self):
 		# discovery RFCOMM channell, prefer HFP.
 		hsp_channel = None
 		generic_channel = None
-		services = bluetooth.find_service(address=addr, uuid=L2CAP_UUID)
+		services = bluetooth.find_service(address=self.addr, uuid=L2CAP_UUID)
 		for svc in services:
 			for c in svc["service-classes"]:
 				service_class = c.lower()
 				if bluetooth.HANDSFREE_CLASS.lower() == service_class:
-					return int(svc["port"])
+					self.channel = int(svc["port"])
+					return
 				elif bluetooth.HEADSET_CLASS.lower() == service_class:
 					hsp_channel = int(svc["port"])
 				elif bluetooth.GENERIC_AUDIO_CLASS.lower() == service_class:
 					generic_channel = int(svc["port"])
 		if hsp_channel:
-			return hsp_channel
-		return generic_channel
+			self.channel = hsp_channel
+		else:
+			self.channel = generic_channel
 
 	def _read_at(self):
 		try:
 			d = self.hfp.recv(1024)
 			logging.debug('> ' + d.decode('utf8'))
 			return d
-		except bluetooth.btcommon.BluetoothError:
+		except bluetooth.btcommon.BluetoothError as e:
+			if str(e) != 'timed out':
+				raise
 			return None
 
 	def _send(self, data):
@@ -131,23 +153,35 @@ class HFPDevice:
 	def _send_error(self):
 		self._send_at(b'ERROR')
 
-	def close(self):
+	def _cleanup(self):
 		if self.audio:
 			self.audio.close()
-		self.hfp.close()
+		if self.hfp:
+			self.hfp.close()
+		self.hfp = None
+		self.audio = None
+
+	def close(self):
 		t = self.pt
 		self.pt = None
 		t.join()
+		self._cleanup()
 
 	def read(self):
 		if not self.audio:
 			return None
-		return self.audio.recv(self.mtu)
+		try:
+			return self.audio.recv(self.mtu)
+		except bluetooth.btcommon.BluetoothError:
+			return None
 
 	def write(self, data):
 		if not self.audio:
-			return 0
-		return self.audio.send(data)
+			return None
+		try:
+			return self.audio.send(data)
+		except bluetooth.btcommon.BluetoothError:
+			return None
 
 
 def demo_ring(hf):
